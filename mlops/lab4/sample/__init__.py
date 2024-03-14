ef1524178ce9b3d9ae2f3ae829f10053d438962a
Alex Beckwith <witzbeck@gmail.com>
2024-03-14 22:46:32 +0000

    refactor to avoid protected namespace

--- mlops/lab4/sample/__init__.py
+++ mlops/lab4/sample/__init__.py
@@ -1,4 +1,9 @@
 from pathlib import Path
+<<<<<<< HEAD:mlops/lab4/sample/__init__.py
+=======
+
+
+>>>>>>> 77c44d7 (refactor to avoid protected namespace):MLOps_Professional/lab4/sample/__init__.py
 this = Path(__file__)
 here = this.parent
 # !/usr/bin/env python
