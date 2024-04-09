ab1d3cc6f9eeac386f12c68977e439c7f3ab8b9d
Alex Beckwith <57322567+witzbeck@users.noreply.github.com>
2024-04-08 23:10:11 -0400

    update components

--- mlops/lab8/app/main.py
+++ mlops/lab8/app/main.py
@@ -1,20 +1,12 @@
-from logging import debug
 from os import getenv
-from pathlib import Path
 
 from fastapi import FastAPI
-from langchain.llms.gpt4all import GPT4All
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from tqdm import tqdm
-from requests import get
-from transformers import AutoModelForCausalLM, AutoTokenizer
 from uvicorn import run
 
 from model.data_model import GenPayload
 from model.picker_bot import PickerBot
 
 app = FastAPI()
-
 
 
 @app.get("/ping")
@@ -30,13 +22,16 @@ async def ping():
 
 
 @app.post("/predict")
-async def predict(payload: GenPayload) -> dict[str: str]:
+async def predict(payload: GenPayload) -> dict[str:str]:
     """Endpoint to perform similarity search and inference"""
-    bot = PickerBot(payload.data, model=gptj)
+    bot = PickerBot(payload.data, model=getenv("MODEL_NAME"))
     bot.data_proc()
     bot.create_vectorstore()
     response = bot.inference(user_input=payload.user_input)
-    return {"message": "Sim Search and Inference Complete", "PickerBot Response": response}
+    return {
+        "message": "Sim Search and Inference Complete",
+        "PickerBot Response": response,
+    }
 
 
 if __name__ == "__main__":
