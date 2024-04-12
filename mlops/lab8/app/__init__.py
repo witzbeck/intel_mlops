5a87246c4061eaff5086859a792446cc618017cb
Alex Beckwith <57322567+witzbeck@users.noreply.github.com>
2024-04-11 22:50:22 -0400

    added init tests and refactored model components

--- mlops/lab8/app/__init__.py
+++ mlops/lab8/app/__init__.py
@@ -1,4 +1,5 @@
 from logging import DEBUG, basicConfig, debug
+from os import environ
 from pathlib import Path
 from sys import path
 
@@ -12,3 +13,19 @@ path.append(str(Path.cwd().parent))
 logconfig = basicConfig(
     level=DEBUG, filename=str(here.parent / f"{here.parent.stem}.log")
 )
+
+if not (dotenv_path := here.parent / ".env").exists():
+    raise FileNotFoundError(f"Could not find {dotenv_path}")
+
+lines = [
+    x.split("#")[0].strip() if "#" in x else x
+    for x in dotenv_path.read_text().splitlines()
+]
+pairs = [x.split("=") for x in lines if x]
+for key, value in pairs:
+    if key not in environ:
+        debug(f"Setting {key} to {value}")
+        environ[key] = value
+    else:
+        debug(f"{key} already set to {environ[key]}")
+
